a8c1b4f1ac0fb51f2a070972f1ec35e8a5da5362
heinrichalven-ux <heinrichalven@gmail.com>
2026-02-04 23:51:49 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 const yesBtn = document.getElementById("yesBtn");
 const noBtn = document.getElementById("noBtn");
 const info = document.getElementById("info");
+const buttons = document.getElementById("buttons");
+const song = document.getElementById("loveSong");
 
 // YES button
 yesBtn.addEventListener("click", () => {
+  // hide buttons
+  buttons.style.display = "none";
+
+  // show info
   info.classList.remove("hidden");
+
+  // play music
+  song.play();
 });
 
 // NO button escapes
@@ -14,6 +23,5 @@ noBtn.addEventListener("touchstart", moveButton);
 function moveButton() {
   const x = Math.random() * 200 - 100;
   const y = Math.random() * 200 - 100;
-
   noBtn.style.transform = `translate(${x}px, ${y}px)`;
 }
